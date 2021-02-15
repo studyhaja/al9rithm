136cc149a5da1390968fb6e5eae173da0efd8a95
hwanil <hikim@koodon.com>
2021-02-15 09:33:08 +0900

    wip:

--- hwanil_kim/note.py
+++ hwanil_kim/note.py
@@ -1,18 +1,17 @@
-def solution(board, moves):
-    stack = []
+def solution(n):
     answer = 0
-    for i in moves:
-        for b in board:
-            if b[i-1] != 0:
-                moved_num = b[i-1]
-                b[i-1] = 0
-                if len(stack) == 0:
-                    stack.append(moved_num)
-                else:
-                    if stack[-1] == moved_num:
-                        stack.pop()
-                        answer += 2
-                    else:
-                        stack.append(moved_num)
-                break
+    for i in range(2, n + 1):
+        if is_prime(i):
+            answer += 1
     return answer
+
+
+def is_prime(num):
+    if num == 2:
+        return True
+    for j in range(2, num):
+        if num % j == 0:
+            return False
+    return True
+
+solution(10)
