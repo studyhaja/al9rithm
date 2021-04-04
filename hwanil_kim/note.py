a320a8816b626457c24c430a47efac587f680458
hwanil <hikim@koodon.com>
2021-04-04 19:58:02 +0900

    wip

--- hwanil_kim/note.py
+++ hwanil_kim/note.py
@@ -1,12 +1,6 @@
-def solution(n, m):
-    greatest_common_factor = 1
-    least_common_multiple = n * m
-    print(least_common_multiple)
-    for i in range(1, min(n,m) + 1):
-        if n % i == 0 and m % i == 0:
-            greatest_common_factor = i
-    least_common_multiple = least_common_multiple / greatest_common_factor
-    return [greatest_common_factor, least_common_multiple]
+a= [1,2,3,4,5]
+a = {1:2, 3:4}
+print(a.items())
 
-a = solution(2, 5)
-print(a)
+
+
